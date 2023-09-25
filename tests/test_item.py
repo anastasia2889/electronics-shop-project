5f25c7998d17223d597b14f00d314c8ee4a1ff8f
Anastasiya Brodskaya <anastasia.brodskaja@yandex.ru>
2023-09-25 16:40:50 +0300

    have written tests in folder test, added code in item.py.

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_calculate_total_price():
+    item = Item('Apple iPhon', 10000, 3)
+    assert item.calculate_total_price() == 30000
+
+def test_apply_discount():
+     item = Item('Apple iPhon', 10000, 2)
+     item.apply_discount()
+     assert item.price == 10000
